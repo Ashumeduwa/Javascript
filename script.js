0897c074445c4456ad3f1f27040096743c65966a
ashumeduwa <ashutosh.sp.sharma@gmail.com>
2022-06-28 20:04:36 +0530

    Working till addition
    Working till addition

--- script.js
+++ script.js
@@ -17,11 +17,10 @@ class mNpotes {
 
         //initiating local storage and saving
 
-
-
         let notesTitle = new Array();
         let notesDetails = new Array();
         let notesPriority = new Array();
+        const cardBody = document.getElementById('card-body')
         notesTitle = JSON.parse(localStorage.getItem('Title'));
         notesDetails = JSON.parse(localStorage.getItem('Details'));
         notesPriority = JSON.parse(localStorage.getItem('Priority'));
@@ -39,34 +38,37 @@ class mNpotes {
         localStorage.setItem('Title', JSON.stringify(notesTitle));
         localStorage.setItem('Details', JSON.stringify(notesDetails));
         localStorage.setItem('Priority', JSON.stringify(notesPriority));
-        console.log(notesTitle);
-        console.log(notesDetails);
-        console.log(notesPriority)
+        cardBody.innerHTML = "";
 
+        this.LoadApps(notesTitle, notesDetails, notesPriority);
 
         //end of initiating local storage and saving
 
         //for loop to print all arrays
+    }
 
-        notesTitle.forEach((Element,index) =>{
-                
+    LoadApps = (notesTitle, notesDetails, notesPriority) => {
+        let mnPriorityArry = ['EOD','Urgent','By week','Normal']
+        mnPriorityArry.forEach(element => {
+            
+            console.log(element);
+        });
 
+        // console.log(mnPriorityArry);
+        notesTitle.forEach((Element, index) => {
+            // console.log(notesTitle[index]);
+            let cardBody = document.getElementById('card-body')
+            let htmlString = `
+            <div class="card-body">
+            <h5 class="card-title">${notesTitle[index]}</h5>
+            <p class="card-text">${notesDetails[index]}</p>
+            <h5 class="card-title">${notesPriority[index]}</h5>
+            <a href="#" class="btn btn-primary">Completed</a>
+          </div>
+            `
+            cardBody.innerHTML += htmlString;
         })
 
-        //for end loop to print all arrays
-
-
-
-        let cardBody = document.getElementById('card-body')
-        let htmlString = `
-        <div class="card-body">
-        <h5 class="card-title">${mnobject.mnTitle}</h5>
-        <p class="card-text">${mnobject.mnDetails}</p>
-        <h5 class="card-title">${mnobject.mnPriority}</h5>
-        <a href="#" class="btn btn-primary">Completed</a>
-      </div>
-        `
-        cardBody.innerHTML += htmlString;
 
     }
 
@@ -98,6 +100,15 @@ btnclick.addEventListener('click', (e) => {
     let formValidation = mnobject.mnValidation(mnobject);
     if (formValidation == "True") {
         mnobject.addValue(mnobject);
+
     }
 
 })
+
+window.onload = (event) => {
+    notesTitle = JSON.parse(localStorage.getItem('Title'));
+    notesDetails = JSON.parse(localStorage.getItem('Details'));
+    notesPriority = JSON.parse(localStorage.getItem('Priority'));
+    let mnobject1 = new mNpotes(notesTitle, notesDetails, notesPriority);
+    mnobject1.LoadApps(notesTitle, notesDetails, notesPriority);
+};
